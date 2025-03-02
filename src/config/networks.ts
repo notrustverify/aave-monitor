f4ea5e5aa1f032847f3231bd976acd29b74d15e5
sven-hash <>
2025-03-02 17:03:06 +0100

    add: aave books

--- src/config/networks.ts
+++ src/config/networks.ts
@@ -1,3 +1,16 @@
+import { 
+  AaveV3Ethereum, 
+  AaveV3Polygon, 
+  AaveV3Avalanche, 
+  AaveV3Optimism, 
+  AaveV3Arbitrum, 
+  AaveV3Gnosis, 
+  AaveV3Base,
+  AaveV3Metis,
+  AaveV3BNB,
+  AaveV3Scroll
+} from '@bgd-labs/aave-address-book';
+
 export interface NetworkConfig {
   name: string;
   chainId: number;
@@ -11,11 +24,26 @@ export interface NetworkConfig {
   blockExplorerUrl: string;
 }
 
+// Define chain IDs for all networks
+const ChainId = {
+  mainnet: 1,
+  polygon: 137,
+  avalanche: 43114,
+  optimism: 10,
+  arbitrum_one: 42161,
+  gnosis: 100,
+  base: 8453,
+  metis_andromeda: 1088,
+  bnb: 56,
+  scroll: 534352,
+  polygon_zkevm: 1101
+};
+
 const networks: Record<string, NetworkConfig> = {
   ethereum: {
     name: 'Ethereum',
-    chainId: 1,
-    contractAddress: '0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2', // Aave V3 Pool on Ethereum
+    chainId: ChainId.mainnet,
+    contractAddress: AaveV3Ethereum.POOL, // Aave V3 Pool on Ethereum from address book
     defaultRpcUrl: 'https://eth.public-rpc.com',
     nativeCurrency: {
       name: 'Ether',
@@ -26,8 +54,8 @@ const networks: Record<string, NetworkConfig> = {
   },
   polygon: {
     name: 'Polygon',
-    chainId: 137,
-    contractAddress: '0x794a61358D6845594F94dc1DB02A252b5b4814aD', // Aave V3 Pool on Polygon
+    chainId: ChainId.polygon,
+    contractAddress: AaveV3Polygon.POOL, // Aave V3 Pool on Polygon from address book
     defaultRpcUrl: 'https://polygon-rpc.com',
     nativeCurrency: {
       name: 'MATIC',
@@ -38,8 +66,8 @@ const networks: Record<string, NetworkConfig> = {
   },
   avalanche: {
     name: 'Avalanche',
-    chainId: 43114,
-    contractAddress: '0x794a61358D6845594F94dc1DB02A252b5b4814aD', // Aave V3 Pool on Avalanche
+    chainId: ChainId.avalanche,
+    contractAddress: AaveV3Avalanche.POOL, // Aave V3 Pool on Avalanche from address book
     defaultRpcUrl: 'https://api.avax.network/ext/bc/C/rpc',
     nativeCurrency: {
       name: 'Avalanche',
@@ -50,8 +78,8 @@ const networks: Record<string, NetworkConfig> = {
   },
   optimism: {
     name: 'Optimism',
-    chainId: 10,
-    contractAddress: '0x794a61358D6845594F94dc1DB02A252b5b4814aD', // Aave V3 Pool on Optimism
+    chainId: ChainId.optimism,
+    contractAddress: AaveV3Optimism.POOL, // Aave V3 Pool on Optimism from address book
     defaultRpcUrl: 'https://mainnet.optimism.io',
     nativeCurrency: {
       name: 'Ether',
@@ -62,8 +90,8 @@ const networks: Record<string, NetworkConfig> = {
   },
   arbitrum: {
     name: 'Arbitrum',
-    chainId: 42161,
-    contractAddress: '0x794a61358D6845594F94dc1DB02A252b5b4814aD', // Aave V3 Pool on Arbitrum
+    chainId: ChainId.arbitrum_one,
+    contractAddress: AaveV3Arbitrum.POOL, // Aave V3 Pool on Arbitrum from address book
     defaultRpcUrl: 'https://arb1.arbitrum.io/rpc',
     nativeCurrency: {
       name: 'Ether',
@@ -74,8 +102,8 @@ const networks: Record<string, NetworkConfig> = {
   },
   gnosis: {
     name: 'Gnosis',
-    chainId: 100,
-    contractAddress: '0xb50201558B00496A145fE76f7424749556E326D8', // Aave V3 Pool on Gnosis
+    chainId: ChainId.gnosis,
+    contractAddress: AaveV3Gnosis.POOL, // Aave V3 Pool on Gnosis from address book
     defaultRpcUrl: 'https://rpc.gnosischain.com',
     nativeCurrency: {
       name: 'xDai',
@@ -86,8 +114,8 @@ const networks: Record<string, NetworkConfig> = {
   },
   base: {
     name: 'Base',
-    chainId: 8453,
-    contractAddress: '0xA238Dd80C259a72e81d7e4664a9801593F98d1c5', // Aave V3 Pool on Base
+    chainId: ChainId.base,
+    contractAddress: AaveV3Base.POOL, // Aave V3 Pool on Base from address book
     defaultRpcUrl: 'https://mainnet.base.org',
     nativeCurrency: {
       name: 'Ether',
@@ -95,8 +123,65 @@ const networks: Record<string, NetworkConfig> = {
       decimals: 18
     },
     blockExplorerUrl: 'https://basescan.org'
+  },
+  metis: {
+    name: 'Metis',
+    chainId: ChainId.metis_andromeda,
+    contractAddress: AaveV3Metis.POOL, // Aave V3 Pool on Metis from address book
+    defaultRpcUrl: 'https://andromeda.metis.io/?owner=1088',
+    nativeCurrency: {
+      name: 'Metis',
+      symbol: 'METIS',
+      decimals: 18
+    },
+    blockExplorerUrl: 'https://andromeda-explorer.metis.io'
+  },
+  bnb: {
+    name: 'BNB Chain',
+    chainId: ChainId.bnb,
+    contractAddress: AaveV3BNB.POOL, // Aave V3 Pool on BNB Chain from address book
+    defaultRpcUrl: 'https://bsc-dataseed.binance.org',
+    nativeCurrency: {
+      name: 'BNB',
+      symbol: 'BNB',
+      decimals: 18
+    },
+    blockExplorerUrl: 'https://bscscan.com'
+  },
+  scroll: {
+    name: 'Scroll',
+    chainId: ChainId.scroll,
+    contractAddress: AaveV3Scroll.POOL, // Aave V3 Pool on Scroll from address book
+    defaultRpcUrl: 'https://rpc.scroll.io',
+    nativeCurrency: {
+      name: 'Ether',
+      symbol: 'ETH',
+      decimals: 18
+    },
+    blockExplorerUrl: 'https://scrollscan.com'
   }
 };
+
+// Add Polygon zkEVM if available in the package
+try {
+  const { AaveV3PolygonZkEvm } = require('@bgd-labs/aave-address-book');
+  if (AaveV3PolygonZkEvm?.POOL) {
+    networks.zkevm = {
+      name: 'Polygon zkEVM',
+      chainId: ChainId.polygon_zkevm,
+      contractAddress: AaveV3PolygonZkEvm.POOL,
+      defaultRpcUrl: 'https://zkevm-rpc.com',
+      nativeCurrency: {
+        name: 'Ether',
+        symbol: 'ETH',
+        decimals: 18
+      },
+      blockExplorerUrl: 'https://zkevm.polygonscan.com'
+    };
+  }
+} catch (e) {
+  console.log('Polygon zkEVM not available in this version of aave-address-book');
+}
 
 export default networks;
 
@@ -113,4 +198,9 @@ export const getNetworkByName = (name: string): NetworkConfig | undefined => {
 // Get all networks as an array
 export const getAllNetworks = (): NetworkConfig[] => {
   return Object.values(networks);
+};
+
+// Get all supported chain IDs
+export const getSupportedChainIds = (): number[] => {
+  return Object.values(networks).map(network => network.chainId);
 }; 
